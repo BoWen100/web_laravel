f8a6efed6eaaa18d0e90971f914710aba11c50b9
Tony <962009493@qq.com>
2020-12-27 01:23:30 +0800

    初始化样式

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Blog APP</title>
+</head>
+<body>
+    <h1>ABOUT</h1>
+</body>
+</html>
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Blog APP</title>
+</head>
+<body>
+    <h1>HELP</h1>
+</body>
+</html>
