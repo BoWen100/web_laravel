792be8d74875a62cb8ed20026a120baec3650031
Tony <962009493@qq.com>
2021-02-28 22:01:28 +0800

    用户显示页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,4 +38,8 @@ class User extends Authenticatable
     ];
 
     protected $table = 'users';
+
+    public function gravatar($size = '100') {
+        return "https://plus-cms-bucket.ws.126.net/2018/09/26/258f3b40903c438e8e78d50628a1ea3e.png";
+    }
 }
